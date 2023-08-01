5bf52a62d20dbd59aefe5463178bebf4445d2272
santiago <santiagobenitez467@gmail.com>
2023-08-01 10:50:36 -0500

    feat: modified files

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
